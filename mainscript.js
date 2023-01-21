ab156c3c668385c2c22fb8fe245695330b677490
Ryan Ertel <ertelryan5@gmail.com>
2023-01-21 10:34:43 -0500

    Implement complete graphical update

--- mainscript.js
+++ mainscript.js
@@ -1,29 +1,45 @@
 //add div to display results with DOM methods
 //display running score and announce winner when 5 points reached
 const buttonRock = document.querySelector('.rock');
+const rockLeft = buttonRock.querySelector('.rockLeft');
+const rockRight = buttonRock.querySelector('.rockRight');
 const buttonPaper = document.querySelector('.paper');
+const paperLeft = buttonPaper.querySelector('.paperLeft');
+const paperRight = buttonPaper.querySelector('.paperRight');
 const buttonScissors = document.querySelector('.scissors');
+const scissorsLeft = buttonScissors.querySelector('.scissorsLeft');
+const scissorsRight = buttonScissors.querySelector('.scissorsRight');
 const divScore = document.querySelector('.score');
 const divWinner = document.querySelector('.winner');
+
 let score = [0,0];
 divScore.textContent = `Score: ${score}`;
 
 buttonRock.addEventListener('click', () => {
     playRound('rock');
+    rockLeft.classList.add('playerSelect');
     checkWin(); 
 });
 buttonPaper.addEventListener('click', () => {
     playRound('paper');
+    paperLeft.classList.add('playerSelect');
     checkWin();
 });
 buttonScissors.addEventListener('click', () => {
     playRound('scissors');
+    scissorsLeft.classList.add('playerSelect');
     checkWin();
 });
 
 function playRound(playerSelection) {
+    rockLeft.classList.remove('playerSelect');
+    rockRight.classList.remove('computerSelect');
+    paperLeft.classList.remove('playerSelect');
+    paperRight.classList.remove('computerSelect');
+    scissorsLeft.classList.remove('playerSelect');
+    scissorsRight.classList.remove('computerSelect');
     let computerSelection = getComputerChoice();
-    computerSelection = 'rock';
+
     if (computerSelection == 'rock' && playerSelection == 'rock') {
         return('draw');
     }
@@ -62,16 +78,24 @@ function playRound(playerSelection) {
 function getComputerChoice() {
     let computerOptions = ['rock','paper','scissors'];
     let computerChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
+    if (computerChoice === 'rock') {
+        rockRight.classList.add('computerSelect');
+    } else if (computerChoice === 'paper') {
+        paperRight.classList.add('computerSelect');
+    } else {
+        scissorsRight.classList.add('computerSelect');
+    }
+
     return(computerChoice);
 }
 
 function checkWin() {
-    divScore.textContent = `Score: ${score}`;
     if (score[0] === 5) {
-        divWinner.textContent = 'Winner: Player';
+        divWinner.textContent = 'Winner last round: Player';
         score = [0,0];
     } else if (score[1] === 5) {
-        divWinner.textContent = 'Winner: Computer';
+        divWinner.textContent = 'Winner last round: Computer';
         score = [0,0];
     }
+    divScore.textContent = `Score: ${score}`;
 }
